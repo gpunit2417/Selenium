e38db902fc3ea3a355b63bfecc7d6460a6707397
gpunit2417 <punitgoyal106@gmail.com>
2025-04-02 11:47:06 +0530

    fetching xpath from inspect

--- selenuimBasic.py
+++ selenuimBasic.py
@@ -10,4 +10,8 @@ time.sleep(2)
 Name = driver.find_element(By.XPATH, "//*[@id='name']")
 Name.send_keys("Punit goyal")
 
+time.sleep(2)
+
+Email = driver.find_element(By.XPATH, "/html/body/div[4]/div[2]/div[2]/div[2]/div[2]/div[2]/div[2]/div/div[4]/div[1]/div/div/div[1]/div[1]/div/div/div/div/div[2]/div[1]/input[2]").send_keys("punitgoyal106@gmail.com")
+
 time.sleep(10)
